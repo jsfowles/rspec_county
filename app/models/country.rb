a0e7816c1cbfe8a2e4ee31f97e82b61c3ec744d6
Jacob Fowles <js.fowles@gmail.com>
2016-06-08 10:03:07 -0600

    working model test

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,2 +1,7 @@
 class Country < ActiveRecord::Base
+  validates_presence_of :name, :population, :language
+
+  def name_language
+    "#{name} has a primary language of: #{language}"
+  end
 end
--- spec/models/country_spec.rb
+++ spec/models/country_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Country, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it{ should validate_presence_of :name }
+    it{ should validate_presence_of :population }
+    it{ should validate_presence_of :language }
+
+  end
+
+
+  describe '#name_language' do
+
+  end
 end
